3a28434a51b75649a0c1a413cca0d6ed579e5166
elPintozo <ricardo.sr.lepe@gmail.com>
2016-07-14 00:24:00 -0400

    se añadió la paleta de colores para la app, se añadió las vio de las primeras dos vistas

---
+++ app/src/main/java/com/thepintozo/myappgym/ultimosEjerciciosController.java
@@ -0,0 +1,13 @@
+package com.thepintozo.myappgym;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ultimosEjerciciosController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ultimos_ejercicios_controller);
+    }
+}
