18aec5b8f4ae7327876e890df700188a3465ccf4
elPintozo <ricardo.sr.lepe@gmail.com>
2016-07-15 01:13:08 -0400

    se añadio una tercera vista(mi rutina), junto a eso se crearon style(estilo para los titulos, subtitulos y texto de los botones) para agilizar la construccion de las vistas restanes

---
+++ app/src/main/java/com/thepintozo/myappgym/MiRutinaController.java
@@ -0,0 +1,13 @@
+package com.thepintozo.myappgym;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MiRutinaController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mi_rutina_controller);
+    }
+}
