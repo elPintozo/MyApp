0ba5b2014f908362e78f8d7e9f43a199b13961b4
elPintozo <ricardo.sr.lepe@gmail.com>
2016-07-15 23:36:54 -0400

    Se finalizaron las 5 principales vistas contempladas en la v1

---
+++ app/src/main/java/com/thepintozo/myappgym/ComenzarEjercicioController.java
@@ -0,0 +1,13 @@
+package com.thepintozo.myappgym;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ComenzarEjercicioController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comenzar_ejercicio_controller);
+    }
+}
---
+++ app/src/main/java/com/thepintozo/myappgym/SeleccionarMusculoController.java
@@ -0,0 +1,13 @@
+package com.thepintozo.myappgym;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SeleccionarMusculoController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccionar_musculo_controller);
+    }
+}
